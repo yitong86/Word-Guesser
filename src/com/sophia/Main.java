14c651c00717ef5a76f652211a3be4a0c537728b
yitong86 <sophiajiang86@hotmail.com>
2022-03-21 17:07:20 -0400

    update

--- src/com/sophia/Main.java
+++ src/com/sophia/Main.java
@@ -1,88 +1,127 @@
 package com.sophia;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     static String letterGuess;
-    static int guessCount = 0 ;
-    static int guessLimit = 7;
+    static int incorrectGuessCount = 0;
+    static int incorrectGuessLimit = 2;
+    static String str ;
+
+    static  String[] playableWords = new String[]{
+            "about", "beginning", "carry",
+            "dance", "early", "false",
+            "glasses", "happy", "issues",
+            "jewels", "knife", "labor",
+            "major", "never", "oceans",
+            "paint", "quick", "radar",
+            "seize", "taste", "under",
+            "value", "wages"
+    };
+
 
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
 
-        String[] playableWords = new String[]{
-                "guessing", "these", "words",
-                "is", "very", "easy"
-        };
-
+//        String[] playableWords = new String[]{
+//                "guessing", "these", "words",
+//                "is", "very", "easy"
+//        };
+        //pick one word
         Random random = new Random();
         // int index = random.nextInt(playableWords.length);
         //int randomIndex = (int) (Math.random() * PLAYABLE_WORDS.length);
         String word = playableWords[(random.nextInt(playableWords.length))];
         System.out.println(word);
 
-
+//list of character
         ArrayList<Character> playerGuesses = new ArrayList<>();
 
         printWordState(word, playerGuesses);
 
+
         while (true) {
+
             getPlayerGuess(keyboard, word, playerGuesses);
+
+          if(printWordState(word,playerGuesses)){
+               break;
+           }
+
         }
+        System.out.println("You win");
+        System.exit(0);
     }
 
     //take the word the player guess and take it to the playerGuesses
     private static void getPlayerGuess(Scanner keyboard, String word, List<Character> playerGuesses) {
-
-        letterGuess = keyboard.nextLine();
-        guessCount++;
-
-        if (guessCount < guessLimit){
-            System.out.println("Please guess a letter in a word !");
-            System.out.println("Guesses made: " + guessCount);
-        }else{
-            System.out.println("You loose");
-            return;
-        }
-
-        playerGuesses.add(letterGuess.charAt(0));
-        printWordState(word, playerGuesses);
-
         //ArrayList convert to string
-        String str = "";
-        for (int i = 0;i < playerGuesses.size();i++) {
-            if (playerGuesses.size() - 1 == i) {
-                str += playerGuesses.get(i) + " ";
+//        str = "";
+//        for (int i = 0;i < playerGuesses.size();i++) {
+//            if (playerGuesses.size() - 1 == i) {
+//                str += playerGuesses.get(i) + " ";
+//            } else {
+//                str += playerGuesses.get(i) + ",";
+//            }
+//        }
+//        System.out.println("Guesses: " + str);
+
+            if (incorrectGuessCount < incorrectGuessLimit ) {
+                System.out.println("Please guess a letter in a word !");
+                letterGuess = keyboard.nextLine();
+                if(!word.contains(letterGuess)) {
+                    incorrectGuessCount++;
+                }
+                    playerGuesses.add(letterGuess.charAt(0));
+                    System.out.println("Incorrect Guesses made: " + incorrectGuessCount + "/" + incorrectGuessLimit);
+
+
             } else {
-                str += playerGuesses.get(i) + ",";
-            }
-        }
-       // System.out.println(str);
-        System.out.println("Guesses: " + str);
+                System.out.println("You run out of turns. ");
+                System.out.println("Would you like to start a game or exit? Please enter 1 to start or 2 to exit.");
+                int input = keyboard.nextInt();
+                    if (input == 2) {
+                        System.out.println("Thank you for your playing");
+                        System.exit(0);
+
+                    } else if (input == 1) {
+                        playerGuesses = new ArrayList<>();
+                        Random random = new Random();
+                         word = playableWords[(random.nextInt(playableWords.length))];
+                        System.out.println(word);
+                        incorrectGuessCount = 0;
+                        letterGuess = keyboard.nextLine();
+
+                    }
+                    }
+
+        System.out.println("Guesses:" + playerGuesses);
+
     }
 
     //if player has guess the letter.print out the letter.if not print out "_"
-    private static void printWordState(String word, List<Character> playerGuesses) {
+    private static boolean printWordState(String word, List<Character> playerGuesses) {
         System.out.print("Word: ");
-        int count = 0;
-
+        int letterCounter = 0;
+        int correctCount = 0;
+        //user guess the correct letter. we print a letter.otherwise print"-"
         for (int i = 0; i < word.length(); i++) {
-                if (playerGuesses.contains(word.charAt(i))) {
-
+                if (playerGuesses.contains(word.charAt(i)) ) {
                     System.out.print(word.charAt(i));
-
+                    correctCount++;
                 } else {
-
                     System.out.print("_");
-                    count++;
+                    letterCounter++;
+
+
+
                 }
             }
-            System.out.println();
-            System.out.print("You need to guess " + count + " letters." );
 
-            System.out.println();
+        System.out.println();
+
+       // System.out.print("You need to guess " + letterCounter + " letters." );
+
+            return (word.length() == correctCount);//true
         }
     }
